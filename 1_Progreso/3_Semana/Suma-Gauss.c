5d0f56004b9671007802c8b5da3446a08f524e28
Mateo Enriquez <mateo.enriquez@udla.edu.ec>
2023-10-12 16:39:49 -0500

    If agregado

---
+++ 1_Progreso/3_Semana/Suma-Gauss.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main (void){
+    
+    int entero, i, entero2;
+
+    printf("Ingrese un numero entero positivo\n");
+    scanf("%d", &entero);
+
+    if (entero<=0)
+    {
+        printf("La suma de gauss es: 0 \n");
+
+    }else{
+        printf("La suma de gauss de %d es:\n", entero);
+        
+        
+    } 
+
+
+    
+    return 0;
+}
