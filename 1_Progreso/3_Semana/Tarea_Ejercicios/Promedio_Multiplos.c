d9f89fbb00b625c122bbc29a9c6bc66e04b9eb38
Mateo Enriquez <mateo.enriquez@udla.edu.ec>
2023-10-16 22:59:17 -0500

    Ejercicio 16_Variables primordiales agregadas

---
+++ 1_Progreso/3_Semana/Tarea_Ejercicios/Promedio_Multiplos.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    //El ejericio nos pide mostrar el promedio entero de los n primeros 
+    //multiplos de 3 para un numero n leido
+
+    int num;
+
+    printf("Ingrese un numero entero \n");
+    scanf("%d", &num);
+    
+    int suma=0, contador=0;
+
+    
+    return 0;
+}
