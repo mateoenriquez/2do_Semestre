81962b4ca594eecdf6bfa0e90f7bb5bee139ca50
Mateo Enriquez <mateo.enriquez@udla.edu.ec>
2023-10-16 23:25:48 -0500

    Codigo competo ejercicio 36

---
+++ 1_Progreso/3_Semana/Tarea_Ejercicios/Tabla_del_5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+    
+    int cinco=5;
+
+    for (int i = 1; i < 12; i++)
+    {
+        printf("%d x %d = %d\n", cinco, i, cinco*i);
+
+    }
+    
+    return 0;
+}
