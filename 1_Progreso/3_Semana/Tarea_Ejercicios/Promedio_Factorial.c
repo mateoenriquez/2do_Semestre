fd3ed1a9da6db2e013b5b7c2758a42b28f9d34d1
Mateo Enriquez <mateo.enriquez@udla.edu.ec>
2023-10-16 23:38:03 -0500

    Declaracion numero y dato de entrada

---
+++ 1_Progreso/3_Semana/Tarea_Ejercicios/Promedio_Factorial.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+    
+    int numero;
+
+    // Solicitar al usuario que ingrese un número entero
+    printf("Ingrese un número entero: ");
+    scanf("%d", &numero);
+
+   
+
+    return 0;
+}
